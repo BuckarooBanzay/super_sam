d0384d6f34616fc712da39d57cb2b5876097c846
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-07-20 20:48:09 +0200

    testing/badge galore

--- luacheck.sh
+++ luacheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for modname in spawn skybox builtin_disable powerups
+for modname in spawn skybox builtin_disable powerups super_sam integration_test
 do
     sh -c "cd mods/${modname} && luacheck ."
 done
