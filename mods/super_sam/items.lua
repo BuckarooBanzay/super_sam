bec40c2fa8f9b8d5abcc3ee7fd2aaea7a9b82c51
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-10-28 19:42:32 +0200

    switch to external artwork

--- mods/super_sam/items.lua
+++ mods/super_sam/items.lua
@@ -1,5 +1,30 @@
 
 minetest.register_craftitem("super_sam:coin", {
-    inventory_image = "super_sam_coin.png",
+    inventory_image = "super_sam_items.png^[sheet:6x5:4,3",
     description = "Coin"
+})
+
+minetest.register_craftitem("super_sam:mushroom_1", {
+    inventory_image = "super_sam_items.png^[sheet:6x5:0,2",
+    description = "Mushroom 1"
+})
+
+minetest.register_craftitem("super_sam:mushroom_2", {
+    inventory_image = "super_sam_items.png^[sheet:6x5:1,2",
+    description = "Mushroom 2"
+})
+
+minetest.register_craftitem("super_sam:mushroom_3", {
+    inventory_image = "super_sam_items.png^[sheet:6x5:2,2",
+    description = "Mushroom 3"
+})
+
+minetest.register_craftitem("super_sam:star_1", {
+    inventory_image = "super_sam_items.png^[sheet:6x5:4,2",
+    description = "Star 1"
+})
+
+minetest.register_craftitem("super_sam:star_2", {
+    inventory_image = "super_sam_items.png^[sheet:6x5:5,2",
+    description = "Star 2"
 })
--- mods/super_sam/readme.md
+++ mods/super_sam/readme.md
@@ -5,3 +5,4 @@ Super sam base mod
 
 * `textures/default_*` CC-BY SA 4.0 https://gitlab.com/zughy-friends-minetest/soothing-32
 * `textures/baked_clay_*` MIT https://notabug.org/TenPlus1/bakedclay
+* `textures/super_sam_items.png` CC-BY SA 4.0 https://opengameart.org/content/mario-like-stuff
