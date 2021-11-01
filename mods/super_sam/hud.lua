782c88ce73673fb5a0be5eed9d60c7c9dc73aa75
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-11-01 19:30:22 +0100

    health / level hud element

--- mods/super_sam/hud.lua
+++ mods/super_sam/hud.lua
@@ -6,6 +6,10 @@ end
 -- player => { name => id }
 local hud_data = {}
 
+local score_position = { x = 0.5, y = 0.05 }
+local health_position = { x = 0.25, y = 0.05 }
+local level_position = { x = 0.75, y = 0.05 }
+
 local function setup_player(player)
     player:hud_set_flags({
         crosshair = true,
@@ -21,7 +25,7 @@ local function setup_player(player)
 
     data.score_img = player:hud_add({
         hud_elem_type = "image",
-        position = { x = 0.5, y = 0.05 },
+        position = score_position,
         text = "super_sam_items.png^[sheet:6x5:4,3",
         offset = {x = 0,   y = 0},
         alignment = { x = -1, y = 0},
@@ -30,12 +34,41 @@ local function setup_player(player)
 
     data.score_text = player:hud_add({
         hud_elem_type = "text",
-        position = { x = 0.5, y = 0.05 },
+        position = score_position,
         number = 0xffffff,
         text = "x0",
         offset = {x = 0,   y = 0},
         alignment = { x = 1, y = 0},
-        scale = {x = 4, y = 4}
+        scale = {x = 2, y = 2}
+    })
+
+    data.health_img = player:hud_add({
+        hud_elem_type = "image",
+        position = health_position,
+        text = "super_sam_items.png^[sheet:6x5:0,2",
+        offset = {x = 0,   y = 0},
+        alignment = { x = -1, y = 0},
+        scale = {x = 2, y = 2}
+    })
+
+    data.health_text = player:hud_add({
+        hud_elem_type = "text",
+        position = health_position,
+        number = 0xffffff,
+        text = "x0",
+        offset = {x = 0,   y = 0},
+        alignment = { x = 1, y = 0},
+        scale = {x = 2, y = 2}
+    })
+
+    data.level_text = player:hud_add({
+        hud_elem_type = "text",
+        position = level_position,
+        number = 0xffffff,
+        text = "Level 0-0",
+        offset = {x = 0,   y = 0},
+        alignment = { x = 1, y = 0},
+        scale = {x = 2, y = 2}
     })
 
     hud_data[player:get_player_name()] = data
