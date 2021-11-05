07736c98f4f864d6ad6769237a717c8afded5301
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-11-05 13:11:16 +0100

    /start command

--- mods/super_sam/level.lua
+++ mods/super_sam/level.lua
@@ -3,6 +3,7 @@
 local levels = {}
 
 function super_sam.register_level(name, def)
+    def.name = name
     levels[name] = def
 end
 
@@ -10,13 +11,22 @@ function super_sam.get_level(name)
     return levels[name]
 end
 
---[[
--- playername => def
-local player_levels = {}
-
-function super_sam.player_set_level(player, name)
+function super_sam.start_level(player, level)
+    player:set_pos(level.start)
 end
 
-function super_sam.player_get_level(player)
-end
---]]
+minetest.register_chatcommand("start", {
+    params = "<levelname>",
+    description = "Start an arbitrary level",
+    privs = { super_sam_builder=true },
+    func = function(name, levelname)
+        local level = super_sam.get_level(levelname)
+        if not level then
+            return false, "Level '" .. levelname .. "' not found"
+        end
+
+        local player = minetest.get_player_by_name(name)
+        super_sam.start_level(player, level)
+        return true, "Level '" .. levelname .. "' started"
+    end
+})
