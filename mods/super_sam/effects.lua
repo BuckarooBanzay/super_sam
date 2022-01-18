d5cdaeb5e93af777f072b9d508e2fde21bc70b0b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-01-18 20:57:54 +0100

    physics override and defaults

--- mods/super_sam/effects.lua
+++ mods/super_sam/effects.lua
@@ -15,6 +15,7 @@ end
 local function register_effect(itemname, name, physicsname, physicsmodifier)
     super_sam.register_on_pickup(itemname, function(player)
         local playername = player:get_player_name()
+        local previous_overrides = player:get_physics_override()
 
         local effects = super_sam.get_player_effects(playername)
         effects[name] = true
@@ -32,11 +33,11 @@ local function register_effect(itemname, name, physicsname, physicsmodifier)
             end
             minetest.sound_play({ name = "super_sam_effect_off", gain = 0.7 }, { to_player = playername }, true)
             player:set_physics_override({
-                [physicsname] = 1
+                [physicsname] = previous_overrides[physicsname]
             })
         end)
     end)
 end
 
 register_effect("super_sam:mushroom_1", "jumping", "jump", 2)
-register_effect("super_sam:mushroom_2", "speed", "speed", 2)
+register_effect("super_sam:mushroom_2", "speed", "speed", 2.5)
--- mods/super_sam/player.lua
+++ mods/super_sam/player.lua
@@ -2,4 +2,7 @@
 -- set the mario skin on join
 minetest.register_on_joinplayer(function(player)
     player_api.set_textures(player, {"sam_mario_skin.png"})
+    player:set_physics_override({
+        speed = 1.8
+    })
 end)
