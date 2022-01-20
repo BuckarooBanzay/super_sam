484d482cf72559d019845c1d9d0d36b759b13475
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-01-20 10:29:22 +0100

    highscore formspec fragment

--- mods/super_sam/highscore.lua
+++ mods/super_sam/highscore.lua
@@ -1,6 +1,6 @@
 
 local storage = minetest.get_mod_storage()
-local max_entries = 10
+local max_entries = 20
 
 local highscore = minetest.deserialize(storage:get_string("highscore")) or {}
 
@@ -41,35 +41,48 @@ function super_sam.update_highscore(playername, score, level)
     storage:set_string("highscore", minetest.serialize(highscore))
 end
 
+function super_sam.get_highscore_formspec_fragment(x, y, size_x, size_y, entries)
+    entries = entries or max_entries
+
+    local list = ""
+    for i, entry in ipairs(highscore) do
+        local color = "#FFFFFF"
+
+        if i == 1 then
+            -- gold
+            color = "#D4AF37"
+        elseif i == 2 then
+            -- silver
+            color = "#C0C0C0"
+        elseif i == 3 then
+            -- bronze
+            color = "#CD7F32"
+        end
+        list = list .. color .. "," .. super_sam.format_score(entry.score) .. "," ..
+            entry.name .. "," .. entry.level .. "," .. os.date('%Y-%m-%d %H:%M:%S', entry.timestamp) .. ","
+
+        if i > entries then
+            break
+        end
+    end
+
+    return [[
+        tablecolumns[color;text;text;text;text]
+        table[]] .. x..","..y..";"..size_x..","..size_y.. [[;items;#999,Score,Playername,Level,Date,]] .. list .. [[]
+    ]]
+end
+
 minetest.register_chatcommand("highscore", {
     description = "Shows the current highscore",
     func = function(name)
 
-        local list = ""
-        for i, entry in ipairs(highscore) do
-            local color = "#FFFFFF"
 
-            if i == 1 then
-                -- gold
-                color = "#D4AF37"
-            elseif i == 2 then
-                -- silver
-                color = "#C0C0C0"
-            elseif i == 3 then
-                -- bronze
-                color = "#CD7F32"
-            end
-            list = list .. color .. "," .. super_sam.format_score(entry.score) .. "," ..
-                entry.name .. "," .. entry.level .. "," .. os.date('%Y-%m-%d %H:%M:%S', entry.timestamp) .. ","
-        end
 
         minetest.show_formspec(name, "highscore", [[
             size[12,12;]
-            label[5,0.1;Highscore top ]] .. max_entries .. [[]
+            label[5,0.1;Highscore top 10]
             button_exit[10,0;2,1;quit;Quit]
-
-            tablecolumns[color;text;text;text;text]
-			table[0,1;11.7,11;items;#999,Score,Playername,Level,Date,]] .. list .. [[]
+            ]] .. super_sam.get_highscore_formspec_fragment(0, 1, 11.7, 11, 10) .. [[
         ]])
     end
 })
