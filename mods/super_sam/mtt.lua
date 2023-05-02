dab0b9f27399a4bca961abc71cde17b4c2b71d2c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-05-02 11:24:04 +0200

    add mod dep check

--- mods/super_sam/mtt.lua
+++ mods/super_sam/mtt.lua
@@ -3,3 +3,14 @@
 mtt.validate_nodenames(minetest.get_modpath("super_sam") .. "/nodenames.txt")
 
 mtt.emerge_area({x=0,y=0,z=0}, {x=48,y=48,z=48})
+
+mtt.register("mod-deps", function(callback)
+    assert(minetest.get_modpath("super_sam_highscore"))
+    assert(minetest.get_modpath("super_sam_hud"))
+    assert(minetest.get_modpath("super_sam_level"))
+    assert(minetest.get_modpath("super_sam_ambience"))
+    assert(minetest.get_modpath("super_sam_map"))
+    assert(minetest.get_modpath("super_sam_nodes"))
+    assert(minetest.get_modpath("super_sam_game_elements"))
+    callback()
+end)
