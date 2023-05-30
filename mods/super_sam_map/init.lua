8c693b1ff53c78a8d54aed1235b069313e37829c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-05-30 20:08:29 +0200

    travelnet mod

--- mods/super_sam_map/init.lua
+++ mods/super_sam_map/init.lua
@@ -2,3 +2,8 @@ mapsync.register_backend("default", {
     type = "fs",
     path = minetest.get_modpath("super_sam_map") .. "/map"
 })
+
+mapsync.register_data_backend({
+    type = "fs",
+    path = minetest.get_modpath("super_sam_map") .. "/data"
+})
